60ebc1bf8a0b90b05aeec5859424f7971bedfc06
cc <cc@192.168.1.183>
2018-11-04 10:11:08 +0800

    commit

---
+++ src/main/java/com/jjx/com/qckj/first/App.java
@@ -0,0 +1,13 @@
+package com.jjx.com.qckj.first;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
